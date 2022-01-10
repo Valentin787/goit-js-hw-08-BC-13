ead3e0b97828e82064f1eba09fb9fb8b5c372db0
Valentin787 <89661234+Valentin787@users.noreply.github.com>
2022-01-10 23:34:37 +0200

    correct -- 02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,33 +1,21 @@
 const  throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
-
 const player = new Vimeo.Player(iframe);
 
 player.on('play', function() {
-  console.log('played the video!');
-  // console.log(`DTA`, data);
 });
-
 player.getVideoTitle().then(function (title) {
-
-    console.log('title:', title);
 });
 
 const info = JSON.parse(localStorage.getItem("videoplayer-current-time")); 
+console.log(info);
+const res = info ? parseInt(info.seconds) : 0;
 
-const res = parseInt(info.seconds);
-console.log(res);
-const onPlay = function(data)  {
-  console.log(`ON Play`, data.seconds);
-  if (data.seconds < 0) {
-    return false
-  }
-  return localStorage.setItem("videoplayer-current-time", JSON.stringify(data))
+const onPlay = data => localStorage.setItem("videoplayer-current-time", JSON.stringify(data))
 
- }
-player.on("timeupdate", throttle(onPlay, 3000));
+player.on("timeupdate", throttle(onPlay, 1000));
 player.on('play', onPlay);
-player.setCurrentTime(res)
+player.setCurrentTime(res )
 
 
 
